0645b9d5652a2a4ae3206a30c6872f743d7b8587
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-03-24 03:56:36 -0700

    Creates Under Construction Page

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,3 +1,4 @@
+import { SECONDARY_COLORS } from "@/lib/colors"
 import Image from "next/image"
 
 const UnderConstruction = () => {
@@ -12,20 +13,28 @@ interface LogoProps {
 }
 
 const Logo = (props: LogoProps) => {
+
+  //REMOVE THIS POINTLESS VARIABLE!
+  const pointless_variable = props.size;
+  console.log(pointless_variable)
+
   return (
-  //   <div className="flex justify-center items-center mt-15 -mb-10">
-  //     <UnderConstruction/>
-  //     Currently Under Construction
-  //     {/* <Image height={props.size} width={props.size} src="./Logo_filler.svg" alt="filler logo while under production" /> */}
-
-  //   </div>
-  // )
-
-  // return (
-    <div className="flex justify-center items-center mt-15 -mb-10">
-      <Image height={props.size} width={props.size} src="./Logo_filler.svg" alt="filler logo while under production" />
+    // <div className="flex justify-center items-center mt-15 -mb-10">
+    <div style={{backgroundColor: SECONDARY_COLORS.YELLOW_107.hex}} className="pt-6 flex flex-col justify-center items-center mt-15 -mb-10">
+      <UnderConstruction/>
+      <div className="font-black p-4">
+      Currently Under Construction
+      </div>
+      {/* <Image height={props.size} width={props.size} src="./Logo_filler.svg" alt="filler logo while under production" /> */}
 
     </div>
+  
+
+  // return (
+    // <div className="flex justify-center items-center mt-15 -mb-10">
+    //   <Image height={props.size} width={props.size} src="./Logo_filler.svg" alt="filler logo while under production" />
+
+    // </div>
   )
 }
 
