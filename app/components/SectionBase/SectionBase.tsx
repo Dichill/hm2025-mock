5613524410a0e519c30916f586fcf595bffb4d0d
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-03-24 03:45:41 -0700

    Adds alternative header text color prop

--- app/components/SectionBase/SectionBase.tsx
+++ app/components/SectionBase/SectionBase.tsx
@@ -1,13 +1,14 @@
 interface SectionBaseProps {
   section_title: string,
   children: React.ReactNode,
-  bg_color: string
+  bg_color: string,
+  alt_text_color?: string
 }
 
 const SectionBase = (props: SectionBaseProps) => {
   return (
     <>
-      <div id={`section-${props.section_title.toLowerCase()}`} style={{ color: "white", height: "100vh", backgroundColor: props.bg_color, scrollMarginTop: "100px" }}>
+      <div id={`section-${props.section_title.toLowerCase()}`} style={{ color: props.alt_text_color ? props.alt_text_color : "white", height: "100vh", backgroundColor: props.bg_color, scrollMarginTop: "100px" }}>
         <h2 style={{ fontSize: "4em", textAlign: "center" }}>{props.section_title}</h2>
         {props.children}</div></>
   )
