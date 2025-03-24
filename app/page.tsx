55fdf93ce340fa4e72d7634d449963d9000ab437
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-03-24 03:49:49 -0700

    Removes large block of commented out code (from merge workflow) and migrates secondary components out of Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,86 +2,29 @@
 
 import { PRIMARY_COLORS, SECONDARY_COLORS, TERTIARY_COLORS } from "@/lib/colors"
 
-import { HackMESA_casing } from "@/lib/colors"
 import CountdownTimer from "./components/CountdownTimer/CountdownTimer"
-import LACC_Graphic from "./components/LACC_Graphic/LACC_Graphic"
-import LACCD_Graphic from "./components/LACCD_Graphic/LACCD_Graphic"
 import MESA_Graphic from "./components/MESA_Graphic/MESA_Graphic"
 import NavBar from "./components/NavBar/NavBar"
 import SectionBase from "./components/SectionBase/SectionBase"
-import useWindowSize from "@/lib/useWindowSize"
-
-
-
-const mobile_size_reference = 800;
-
-const HeroHeader = () => {
-  const {/* height */ width } = useWindowSize();
-
-  return (
-    <>
-      <header style={{ textAlign: "center", marginTop: width > mobile_size_reference ? "0em" : "4em" }}>
-        <h1 style={{ fontSize: width > mobile_size_reference ? "5em" : "2em", fontWeight: "900", color: `${PRIMARY_COLORS.GREY_432.hex}` }}>{HackMESA_casing} 2025</h1>
-      </header>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ display: "inline-block" }}>
-          <div style={{ display: width > mobile_size_reference ? "flex" : "block", justifyContent: "center", alignItems: "center", padding: "1em" }}>
-            {width > mobile_size_reference && <MESA_Graphic width={150} />}
-            {width <= mobile_size_reference && <div style={{ display: "flex", justifyContent: "center" }}><MESA_Graphic width={150} /></div>}
-
-
-
-            <p style={{ fontSize: width > mobile_size_reference ? "2em" : "1em", position: "relative", top: "-6px", paddingLeft: "7px" }}>sponsored, LACCD student Hackathon</p>
-          </div>
-        </div>
-      </div></>
-  )
-}
-
-const Trifecta_Graphic = () => {
-  const {/* height */ width } = useWindowSize();
-
-  if (width > mobile_size_reference) {
-    return (
-      <>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px" }}>
-          <div><LACC_Graphic width={240} /></div>
-          <div><LACCD_Graphic width={180} /></div>
-          <div style={{ position: "relative", top: "15px" }}><MESA_Graphic width={260} /></div>
-        </div></>
-    )
-  }
-
-  return (
-    <>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px" }}>
-        <div><LACC_Graphic width={100} /></div>
-        <div><LACCD_Graphic width={100} /></div>
-        <div style={{ position: "relative", top: "15px" }}><MESA_Graphic width={100} /></div>
-      </div></>
-  )
-}
+import HeroHeader from "./components/HeroHeader/HeroHeader";
+import TrifectaGraphic from "./components/TrifectaGraphic/TrifectaGraphic";
 
 function App() {
-  const {/* height */ width } = useWindowSize();
-
-
-
+  // const {/* height */ width } = useWindowSize();
 
   return (
     <>
       {/* {width + " " + height} */}
 
-      {width > 800 && <NavBar />}
+      <NavBar />
+
       <HeroHeader />
 
-
-
-      <Trifecta_Graphic />
-
-
-      <div style={{ marginTop: "3em", marginBottom: "10em" }}><CountdownTimer /></div>
-      <SectionBase section_title="About" bg_color={`${SECONDARY_COLORS.YELLOW_107.hex}`}>
+      <CountdownTimer />
+      <div className="p-10">
+        <TrifectaGraphic />
+      </div>
+      <SectionBase section_title="About" bg_color={`${SECONDARY_COLORS.YELLOW_107.hex}`} alt_text_color={PRIMARY_COLORS.GREY_432.hex}>
         <MESA_Graphic width={200} />
       </SectionBase>
       <SectionBase section_title="Schedule" bg_color={`${SECONDARY_COLORS.RHODAMINE_RED.hex}`} >
@@ -94,108 +37,20 @@ function App() {
         {" "}
       </SectionBase>
 
+      <SectionBase section_title="FAQ" bg_color={`${PRIMARY_COLORS.WARM_RED.hex}`}>
+        {" "}
+      </SectionBase>
+
+      <SectionBase section_title="Team" bg_color={"white"} alt_text_color={PRIMARY_COLORS.GREY_432.hex}>
+        {" "}
+      </SectionBase>
+
       <footer style={{ padding: "3em" }}>
 
-        <Trifecta_Graphic />
+        <TrifectaGraphic />
       </footer>
     </>
   )
 }
 
 export default App;
-
-// const Trifecta_Graphic = () => {
-//     return (
-//         <>
-//             <div
-//                 style={{
-//                     display: "flex",
-//                     justifyContent: "center",
-//                     alignItems: "center",
-//                     gap: "20px",
-//                 }}
-//             >
-//                 <div>
-//                     <LACC_Graphic width={240} />
-//                 </div>
-//                 <div>
-//                     <LACCD_Graphic width={180} />
-//                 </div>
-//                 <div style={{ position: "relative", top: "15px" }}>
-//                     <MESA_Graphic width={260} />
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-
-
-// function App() {
-//     return (
-//         <>
-//             <NavBar />
-//             <HeroHeader />
-
-//             <div style={{ display: "flex", justifyContent: "center" }}>
-//                 <div style={{ display: "inline-block" }}>
-//                     <div
-//                         style={{
-//                             display: "flex",
-//                             justifyContent: "center",
-//                             alignItems: "center",
-//                             padding: "1em",
-//                         }}
-//                     >
-//                         <MESA_Graphic width={150} />
-//                         <p
-//                             style={{
-//                                 fontSize: "2em",
-//                                 position: "relative",
-//                                 top: "-6px",
-//                                 paddingLeft: "7px",
-//                             }}
-//                         >
-//                             sponsored Hackathon
-//                         </p>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             <Trifecta_Graphic />
-
-//             <div style={{ marginTop: "3em", marginBottom: "10em" }}>
-//                 <CountdownTimer />
-//             </div>
-//             <SectionBase
-//                 section_title="About"
-//                 bg_color={`${SECONDARY_COLORS.YELLOW_107.hex}`}
-//             >
-//                 <MESA_Graphic width={200} />
-//             </SectionBase>
-//             <SectionBase
-//                 section_title="Schedule"
-//                 bg_color={`${SECONDARY_COLORS.RHODAMINE_RED.hex}`}
-//             >
-//                 stuff
-//             </SectionBase>
-//             <SectionBase
-//                 section_title="Location"
-//                 bg_color={`${TERTIARY_COLORS.GREEN_367.hex}`}
-//             >
-//                 stuff
-//             </SectionBase>
-//             <SectionBase
-//                 section_title="Sponsors"
-//                 bg_color={`${TERTIARY_COLORS.PURPLE_2655.hex}`}
-//             >
-//                 stuff
-//             </SectionBase>
-
-//             <footer style={{ padding: "3em" }}>
-//                 <Trifecta_Graphic />
-//             </footer>
-//         </>
-//     );
-
-// }
