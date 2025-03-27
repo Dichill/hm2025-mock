6067355814463b3ccb2b9ae9c3e8b7acf47dd2cb
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-03-27 02:35:35 -0700

    Adjusts map iframe

--- app/components/LocationMap/LocationMap.tsx
+++ app/components/LocationMap/LocationMap.tsx
@@ -1,6 +1,6 @@
 const LocationMap = () => {
   return (
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.3544211511157!2d-118.2957693184776!3d34.08605992502113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c757563c5ab5%3A0x160c1c4565231a5!2sStudent%20Services%20Center%2C%20Los%20Angeles%2C%20CA%2090029!5e0!3m2!1sen!2sus!4v1743065916824!5m2!1sen!2sus" width="600" height="300" style={{border:0, margin: "2em"}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.3544211511157!2d-118.2957693184776!3d34.08605992502113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c757563c5ab5%3A0x160c1c4565231a5!2sStudent%20Services%20Center%2C%20Los%20Angeles%2C%20CA%2090029!5e0!3m2!1sen!2sus!4v1743065916824!5m2!1sen!2sus" width="80%" height="300" style={{border:0, margin: "2em"}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
   )
 }
 
