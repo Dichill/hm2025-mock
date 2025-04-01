83ba3e2fb02371e5cbf915b8538f36e1c09e06a4
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-04-01 02:52:22 -0700

    Adds centralized location for hyperlink destinations

---
+++ lib/link_base.tsx
@@ -0,0 +1,9 @@
+export const home = "https://hackmesa.com/";
+export const github = "https://github.com/HACKMESA";
+export const discord = "https://discord.com/invite/VU6anPtkMV"; 
+
+export const instagram = "#";
+export const tiktok = "#";
+
+export const lacc = "https://www.lacc.edu/";
+export const laccd = "https://www.laccd.edu/";
