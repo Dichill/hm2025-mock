e024e343dcd846034d68d4bc1b3b8d3e2573e3f7
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-04-01 09:40:59 -0700

    Adds conditional render for fontSize; was breaking mobile

--- app/components/SectionBase_HeroText.tsx
+++ app/components/SectionBase_HeroText.tsx
@@ -1,12 +1,17 @@
 import { SECONDARY_COLORS } from "@/lib/colors";
+import useWindowSize from "@/lib/useWindowSize";
 
 interface SectionBase_HeroText_Props {
   text: string
 }
 
 const SectionBase_HeroText = (props: SectionBase_HeroText_Props) => {
+
+  const {width} = useWindowSize();
+
+
   return (
-    <h2 style={{ fontSize: 100, fontWeight: "800", color: SECONDARY_COLORS.YELLOW_107.hex, textShadow: "10px 10px 10px black" }} className="text-white">{props.text}</h2>
+    <h2 style={{ fontSize: width > 500 ? 100 : 50, fontWeight: "800", color: SECONDARY_COLORS.YELLOW_107.hex, textShadow: "10px 10px 10px black" }} className="text-white">{props.text}</h2>
   )
 }
 
