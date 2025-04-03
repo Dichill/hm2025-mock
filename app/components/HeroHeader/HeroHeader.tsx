89b9c0bef4f39744ae052666a9c953e31f6dd530
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-04-03 11:08:06 -0700

    Adds updated graphics

--- app/components/HeroHeader/HeroHeader.tsx
+++ app/components/HeroHeader/HeroHeader.tsx
@@ -1,8 +1,10 @@
-import { HackMESA_casing } from "@/lib/colors"
+import { HackMESA_casing, TERTIARY_COLORS } from "@/lib/colors"
 import { register } from "@/lib/link_base"
+// import useWindowSize from "@/lib/useWindowSize"
 
 
 const HeroHeader = () => {
+  // const {width} = useWindowSize();
 
   return (
 
@@ -11,7 +13,7 @@ const HeroHeader = () => {
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">{HackMESA_casing} 2025</h1>
         <p className="mb-8 text-3xl font-normal lg:text-xl sm:px-16 lg:px-48 ">LACCD student Hackathon</p>
-        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+        <div style={{ backgroundColor: TERTIARY_COLORS.PURPLE_2655.hex, maxWidth: "50%", margin: "auto", }} className="flex p-4 flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <a href={`${register}`} style={{color: "#2c2c2c"}} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg focus:ring-4">
             Register
             <svg transform="scale(1.5)" className="w-3.5 h-3.5 ms-2 rtl:rotate-180 ml-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
