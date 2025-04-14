394a2669bac24ceae4a661aee26f77fdccf3db48
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-04-13 19:05:23 -0700

    revert Image to img for Image_Overlay (Image is complicated)

--- app/components/Image_Overlay/Image_Overlay.tsx
+++ app/components/Image_Overlay/Image_Overlay.tsx
@@ -1,33 +1,19 @@
-import Image from "next/image";
-
 interface Image_OverlayProps {
-    width: string;
-    height: string;
-    margin: string;
-    display: string;
-    source: string;
-    float: "left" | "right" | "none" | undefined;
-    opacity: number;
-}
-
-const Image_Overlay = (props: Image_OverlayProps) => {
+    width: string,
+    height: string,
+    margin: string,
+    display: string,
+    source: string,
+    float: "left" | "right" | "none" | undefined,
+    opacity: number
+  }
+  
+  const Image_Overlay = (props: Image_OverlayProps) => {
+  
     return (
-        <Image
-            src={props.source}
-            alt="MESA Student tinkering with sophisticated electronics"
-            width={parseInt(props.width) || 500}
-            height={parseInt(props.height) || 500}
-            style={{
-                opacity: `${props.opacity}%`,
-                float: props.float,
-                margin: props.margin,
-                width: props.width,
-                height: props.height,
-                display: props.display,
-            }}
-            className="object-cover"
-        />
-    );
-};
-
-export default Image_Overlay;
+      <img src={props.source} style={{opacity: `${props.opacity}%`, float: props.float, margin: props.margin, width: props.width, height: props.height, display: props.display}} className="object-cover" alt="MESA Student tinkering with sophisticated electronics" />
+  )
+  }
+  
+  export default Image_Overlay
+  
